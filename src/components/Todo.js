d40f75ab638d07b68bce7c5221979183b96b3521
Suraj Bhattacharya <bhattacharyasuraj2@gmail.com>
2019-10-15 10:47:02 +0530

    Handling Object with useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
 
 const Todo = props => {
-    const [todoName, setTodoName] = useState("");
+    const [todoName, setTodoName] = useState({ firstName: "", lastName: "" });
     const [todoList, setTodoList] = useState([]);
     const inputHandler = (event) => {
-        setTodoName(event.target.value)
+        let { id, value } = event.target
+        setTodoName({ ...todoName, [id]: value })
     }
     const todoAddhandler = () => {
         setTodoList(todoList.concat(todoName))
@@ -13,13 +14,24 @@ const Todo = props => {
         <React.Fragment>
             <input
                 type="text"
-                placeholder="Todo"
+                id="firstName"
+                placeholder="First Name"
                 onChange={inputHandler}
-                value={todoName}
+                value={todoName.firstName}
+            />
+            <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                onChange={inputHandler}
+                value={todoName.lastName}
             />
             <button type="button" onClick={todoAddhandler}>Add</button>
             <ul>
-                {todoList.map((todo, index) => <li key={`${todo}_${index}`}>{todo}</li>)}
+                {todoList.map((todo, index) => <li
+                    key={`${todo.firstName}_${index}`}>
+                    {todo.firstName}-{todo.lastName}
+                </li>)}
             </ul>
         </React.Fragment >
     )
